70efe4a76be5c6a905e6e36866643e60af22792d
abderral <at.gfi.abderrahim@sesam-vitale.fr>
2022-11-04 11:56:49 +0100

    add heroku configs

---
+++ server.js
@@ -0,0 +1,33 @@
+/* const express = require('express');
+const helmet = require('helmet');
+const path = require('path');
+const app = express();
+app.use(helmet());
+app.use(
+  helmet.contentSecurityPolicy({
+    useDefaults: false,
+    directives: {
+      'default-src': ["'self'"],
+      'script-src': ["'self'", "'unsafe-inline'"],
+      'style-src': ["'self'", "'unsafe-inline'", 'https://fonts.googleapis.com/icon'],
+      'font-src': ["'self'", 'https://fonts.gstatic.com/'],
+      'img-src': ["'self'", 'data:'],
+    },
+  })
+);
+app.use(express.static(__dirname + '/dist/angular-bootstrap-toast-service'));
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname + '/dist/angular-bootstrap-toast-service/index.html'));
+});
+app.listen(process.env.PORT || 8080); */
+
+let express = require('express');
+
+let app = express();
+
+app.use(express.static(__dirname + '/dist/digital-banking-web'));
+
+app.get('/*', (req, res) => {
+  res.sendFile(__dirname + '/dist/digital-banking-web/index.html');
+});
+app.listen(process.env.PORT || 8080);
